7b0d1a4dd187196587ed521130bd574c47a97768
Daniel Svensson <daniel.svensson@netnod.se>
2019-06-27 23:30:07 +0200

    Add kbd style.

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -52,6 +52,25 @@ a {
   }
 }
 
+kbd {
+  -moz-border-radius:3px;
+  -moz-box-shadow:0 1px 0 rgba(0,0,0,0.2),0 0 0 2px #fff inset;
+  -webkit-border-radius:3px;
+  -webkit-box-shadow:0 1px 0 rgba(0,0,0,0.2),0 0 0 2px #fff inset;
+  background-color:#f7f7f7;
+  border:1px solid #ccc;
+  border-radius:3px;
+  box-shadow:0 1px 0 rgba(0,0,0,0.2),0 0 0 2px #fff inset;
+  color:#333;
+  display:inline-block;
+  font-family: "Fira Code";
+  line-height: 1.55;
+  line-height:1.4;
+  margin:0 .1em;
+  padding:.1em .6em;
+  text-shadow:0 1px 0 #fff;
+}
+
 /* base16-prism (https://github.com/atelierbram/base16-prism)
  * by Bram de Haan (http://atelierbramdehaan.nl)
  * Nord scheme by arcticicestudio
